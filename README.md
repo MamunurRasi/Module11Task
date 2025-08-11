63de08973671190c346c7a1a540ad2cb98ed9970
Mamunur Rasid <mamunurduet1029@gmail.com>
2025-08-12 00:51:50 +0600

    Task has add

---
+++ README.md
@@ -0,0 +1,2 @@
+# Module11Task
+this repository is for my ostad AI course module 11 project
---
+++ app/schemas.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+class InputFeatures(BaseModel):
+    age: float
+    sex: int
+    cp: int
+    trestbps: float
+    chol: float
+    fbs: int
+    restecg: int
+    thalach: float
+    exang: int
+    oldpeak: float
+    slope: int
+    ca: int
+    thal: int
+
+class OutputPrediction(BaseModel):
+    prediction: int
+    probability: float
